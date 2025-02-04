0248e432535db2f9eb05c02ad8d7963f5f1e5cca
admin <admin@tuit.local>
2025-02-04 12:04:32 +0500

    Sonlarning ekubini hisoblovchi dastur bu

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def ekub(x,y):
+    ekub=1
+    if x%y==0:
+        return y
+
+    for k in range(int(x/2),0,-1):
+        if x%k==0 and y%k==0:
+            return k
+            break
+        
+
+print(ekub(6,4))
+print(ekub(16,12))
